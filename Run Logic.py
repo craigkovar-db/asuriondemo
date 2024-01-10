fbbc94917d8fc73d94d32d05fd157f97b7aa3b4c
Craig Kovar <craig.kovar@databricks.com>
2024-01-10 08:18:06 -0600

    Updated message

--- Run Logic.py
+++ Run Logic.py
@@ -9,4 +9,4 @@ full_path = os.path.join(path,dir)
 with open(f'{full_path}/new_file.txt', "r") as f:
     name_string = f.readline()
 
-print(f"Hello {name_string}, this is initial message!")
+print(f"Hello {name_string}, this is an updated message!")
